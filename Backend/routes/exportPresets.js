799547a4e7171c2e6c3875fe234f2882acfd56fe
Zeeshan Mustafa <zeeshanbrainyte123@gmail.com>
2025-06-16 15:39:27 +0500

    .

--- Backend/routes/exportPresets.js
+++ Backend/routes/exportPresets.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const exportPresetsController = require('../controllers/exportPresets');
+const exportPresetsController = require('../controllers/presets');
 
 
 router.get('/color-palettes', exportPresetsController.getColorPalettes);
